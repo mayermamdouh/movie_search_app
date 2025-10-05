8b26e35762193b999aa56339e2d1d8cc9e8450cc
mayermamdouh <m.noshy485@gmail.com>
2025-10-05 18:55:06 +0300

    Add lazy loading for MovieDetails page

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,26 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-
-const MovieDetails = lazy(() => import("../pages/MovieDetails"));
 import Layout from "../Layout";
 import { lazy, Suspense } from "react";
 import CircleLoader from "../components/CircleLoader";
 
+const MovieDetails = lazy(() => import("../pages/MovieDetails"));
+
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Suspense
-        fallback={
-          <div className="flex items-center justify-center w-full h-screen">
-            <CircleLoader />
-          </div>
-        }
-      >
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center w-full h-screen">
+          <CircleLoader />
+        </div>
+      }
+    >
+      <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/movie/:id" element={<MovieDetails />} />
         </Route>
-      </Suspense>
-    </Routes>
+      </Routes>
+    </Suspense>
   );
 }
